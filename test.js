ddd84a6ed2d7fd386ddae3b4a1ecfff64ef2aa38
Max Schmitt <max@schmitt.mx>
2020-09-21 10:01:22 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+// @ts-check
+const playwright = require("playwright");
+
+(async () => {
+  // Try to add 'firefox' to the list ↓
+  for (const browserType of ['chromium']) {
+    /** @type {import('playwright').Browser} */
+    const browser = await playwright[browserType].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://whatsmyuseragent.org/');
+    await page.screenshot({ path: `example-${browserType}.png` });
+    await browser.close();
+  }
+})();
